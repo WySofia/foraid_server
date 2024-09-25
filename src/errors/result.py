7259108121ac5e5be1893f5454bde899585a87f0
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2024-09-25 10:48:23 -0500

    fix(errors): Go back to my implementation

--- src/errors/result.py
+++ src/errors/result.py
@@ -1,54 +1,52 @@
-from typing import Any, Callable
+from typing import Generic, TypeVar, Callable, Any
+
 from src.errors.custom_error import CustomError, ErrorTag, handle_error
 
+T = TypeVar("T")
+E = TypeVar("E", bound=CustomError)
 
-class Result[T, E]:
+
+class Result(Generic[T, E]):
     pass
 
 
-class Ok[T, E](Result[T, E]):
+class Ok(Result[T, E]):
     def __init__(self, value: T):
         self.value = value
 
     def is_ok(self) -> bool:
         return True
-
     def is_err(self) -> bool:
         return False
-
     def unwrap(self) -> T:
         return self.value
-
     def __repr__(self) -> str:
         return f"Ok({self.value})"
 
 
-class Err[T, E](Result[T, E]):
+class Err(Result[T, E]):
     def __init__(self, error: E):
         self.error = error
 
     def is_ok(self) -> bool:
         return False
-
     def is_err(self) -> bool:
         return True
-
     def unwrap_err(self) -> E:
         return self.error
-
     def __repr__(self) -> str:
         return f"Err({self.error})"
 
 
-def ok[T](value: T) -> Ok[T, Any]:
+def ok(value: T) -> Ok[T, Any]:
     return Ok(value)
 
 
-def err[E](error: E) -> Err[Any, E]:
+def err(error: E) -> Err[Any, E]:
     return Err(error)
 
 
-def map_result[T, E](result: Result[T, E], func: Callable[[T], Any]) -> Result[Any, E]:
+def map_result(result: Result[T, E], func: Callable[[T], Any]) -> Result[Any, E]:
     if isinstance(result, Ok):
         try:
             return Ok(func(result.value))
@@ -58,9 +56,7 @@ def map_result[T, E](result: Result[T, E], func: Callable[[T], Any]) -> Result[A
         return result
 
 
-def map_err_result[
-    T, E
-](result: Result[T, E], func: Callable[[E], Any]) -> Result[T, Any]:
+def map_err_result(result: Result[T, E], func: Callable[[E], Any]) -> Result[T, Any]:
     if isinstance(result, Err):
         try:
             return Err(func(result.error))
