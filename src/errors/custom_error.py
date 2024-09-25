042ea69de3dafeb14327c17db6a023a418d53d6a
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2024-09-25 10:05:30 -0500

    refactor(errors): Update custom error tags

--- src/errors/custom_error.py
+++ src/errors/custom_error.py
@@ -5,17 +5,9 @@ from enum import Enum
 
 
 class ErrorTag(Enum):
-    VALIDATION_ERROR = "ValidationError"
     LOGIC_ERROR = "LogicError"
     DATA_ERROR = "DataError"
-    API_ERROR = "ApiError"
     UNKNOWN_ERROR = "UnknownError"
-
-
-@dataclass
-class ValidationError:
-    tag: ErrorTag = ErrorTag.VALIDATION_ERROR
-    message: str = "Error validating fields types"
 
 
 @dataclass
@@ -31,26 +23,18 @@ class DataError:
 
 
 @dataclass
-class ApiError:
-    tag: ErrorTag = ErrorTag.API_ERROR
-    message: str = "Error fetching data"
-
-
-@dataclass
 class UnknownError:
     tag: ErrorTag = ErrorTag.UNKNOWN_ERROR
     message: str = "Unknown error"
 
 
-CustomError = Union[ValidationError, LogicError, DataError, ApiError, UnknownError]
+CustomError = Union[LogicError, DataError, UnknownError]
 
 
 def create_error(tag: ErrorTag, message: str) -> CustomError:
     error_classes = {
-        ErrorTag.VALIDATION_ERROR: ValidationError,
         ErrorTag.LOGIC_ERROR: LogicError,
         ErrorTag.DATA_ERROR: DataError,
-        ErrorTag.API_ERROR: ApiError,
         ErrorTag.UNKNOWN_ERROR: UnknownError,
     }
 
@@ -62,10 +46,8 @@ def handle_error(tag: ErrorTag, error: Any) -> CustomError:
     message = get_error_message(error)
 
     error_messages = {
-        ErrorTag.VALIDATION_ERROR: f"Error validating fields types: {message}",
         ErrorTag.LOGIC_ERROR: f"Error in logical operations: {message}",
         ErrorTag.DATA_ERROR: f"Error in data processing: {message}",
-        ErrorTag.API_ERROR: f"Error fetching data: {message}",
         ErrorTag.UNKNOWN_ERROR: f"Unknown error: {message}",
     }
 
@@ -74,4 +56,9 @@ def handle_error(tag: ErrorTag, error: Any) -> CustomError:
 
 
 def get_error_message(error: Any) -> str:
-    return str(error) if isinstance(error, Exception) else "An unknown error occurred."
+    if isinstance(error, Exception):
+        return str(error)
+    elif isinstance(error, str):
+        return error
+    else:
+        return "An unknown error occurred."
