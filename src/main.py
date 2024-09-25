5b9c54865147261e22efbbc414334bc7056951cc
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2024-09-25 00:52:23 -0500

    tests(errors): Use simple examples to test errors

--- src/main.py
+++ src/main.py
@@ -1,6 +1,28 @@
+import math
+
+from src.errors.custom_error import CustomError, ErrorTag, handle_error
+from src.errors.result import Err, Ok, Result
+
+
 def add(x: int, y: int) -> int:
     return x + y
 
 
 def is_even(x: int) -> bool:
     return x % 2 == 0
+
+
+def safe_div(a: float, b: float) -> Result[float, CustomError]:
+    if b == 0:
+        return Err(handle_error(ErrorTag.LOGIC_ERROR, "No division by zero"))
+    return Ok(a / b)
+
+
+def safe_sqrt(x: float) -> Result[float, CustomError]:
+    if x < 0:
+        return Err(
+            handle_error(
+                ErrorTag.VALIDATION_ERROR, "No square root of negative numbers"
+            )
+        )
+    return Ok(math.sqrt(x))
