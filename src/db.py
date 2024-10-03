e19c516b1be6690cf159ed3a293221e759e2d654
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2024-10-02 23:40:50 -0500

    refactor(db config): Update database connection

---
+++ src/db.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+SessonLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
