8eff412af1c1ae9c2039c433095eff8a9d48ef0b
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2024-10-08 23:27:44 -0500

    feat(models): Create identikit model

---
+++ src/models/identikit.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Date, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from src.db import Base
+
+
+class Identikit(Base):
+    __tablename__ = "identikits"
+
+    id_identikit = Column(Integer, primary_key=True, index=True)
+    id_caso = Column(Integer, ForeignKey("casos.id_caso"), nullable=False)
+    fecha_creacion = Column(Date, nullable=False)
+    id_metodo_creacion = Column(
+        Integer, ForeignKey("metodo_creacion.id_metodo_creacion"), nullable=False
+    )
+    imagen = Column(String(255))
+
+    caso = relationship("Caso", back_populates="identikits")
+    metodo_creacion = relationship("MetodoCreacion", back_populates="identikits")
+    caracteristicas = relationship("Caracteristica", back_populates="identikit")
