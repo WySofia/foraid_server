4b57d24c764c5c9f329ee9ee174be19392bce6d2
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2024-10-08 23:28:05 -0500

    feat(models): Create tipo_caso model

---
+++ src/models/tipo_caso.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from src.db import Base
+
+
+class TipoCaso(Base):
+    __tablename__ = "tipo_caso"
+
+    id_tipo_caso = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String(50), unique=True, nullable=False)
+
+    casos = relationship("Caso", back_populates="tipo_caso")
