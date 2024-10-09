76152719843fe91359f2667bd0aa73035145fc8c
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2024-10-08 23:27:27 -0500

    feat(models): Create Usuario model

---
+++ src/models/usuario.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from src.db import Base
+
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+
+    id_usuario = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String(50), nullable=False)
+    apellido = Column(String(50), nullable=False)
+    correo = Column(String(100), unique=True, nullable=False, index=True)
+    contrasenia_hash = Column(String, nullable=False)
+    cargo = Column(String(50))
+    rango = Column(String(50))
+
+    casos = relationship("Caso", back_populates="usuario")
